0484f4c782ff02f098527e512ad425b86c7e11e3
Matheus <matheuswe2010@hotmail.com>
2025-01-21 17:47:32 -0300

    project created

---
+++ src/main/java/com/matheus/gamelogger/GameloggerApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.gamelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameloggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/gamelogger/GameloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.gamelogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
